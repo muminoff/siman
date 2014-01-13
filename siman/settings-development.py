67cd6c1b6c0f409dd55f4bdca914b8fb04411156
muminoff <smuminov@gmail.com>
2014-01-13 17:15:31 +0900

    Integrate initial frontend

--- siman/settings-development.py
+++ siman/settings-development.py
@@ -2,7 +2,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'mv4sz+4t*o7!r3mn+25=o87w^h6t@-wn#*ua*e0a7ebc*sx=ka'
 DEBUG = True
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -11,8 +11,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
-
+) 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,6 +39,7 @@ USE_I18N = True
 USE_L10N = True 
 USE_TZ = True 
 STATIC_URL = '/static/'
+TEMPLATE_DIR = '/frontend/'
 INSTALLED_APPS += ( 'core', )
 INSTALLED_APPS += ( 'south', )
 INSTALLED_APPS += ( 'rosetta', )
